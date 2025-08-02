096cff680b71c142135e3b609e087a36238299ac
Satwik Ghildiyal <satwikghildiyal@Satwiks-MacBook-Air.local>
2025-08-03 00:30:32 +0530

    testing the inference for commands other than create wallet

---
+++ inference_test.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'  
+    
+    // const userMessage = 'I want to transfer SOL'
+
+    const userMessage = 'I want to transfer Solana to: 5XhKfpXHWpp6sPmt1LFXG4wgovncwmbqYiCnhcARuBKm'
+
+    const systemPrompt = process.env.systemPrompt
+
+    const finalPrompt = `###System: ${systemPrompt} 
+    
+    
+    ###User : ${userMessage}`
+
+    const llmResp = await fetch('http://localhost:11434/api/generate', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          model: 'llama2',
+          prompt: finalPrompt,
+          stream: false
+        
+        })
+    })
+
+    const data = await llmResp.json()
+
+    const llmOutput = data.response.trim()
+
+    console.log(llmOutput)
+
+    const intent = JSON.parse(llmOutput)
+
+    console.log(intent)
